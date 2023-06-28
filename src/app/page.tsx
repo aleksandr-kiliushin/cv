debf17e1b37654817b56d8939ab39420a06b36a1
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-28 11:17:18 +0400

    Add main, global styles and contacts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,39 @@
 import { FC } from "react"
 
+import classes from "./page.module.css"
+
 const Cv: FC = () => {
   return (
     <main>
-      <h1>Aleksandr Kiliushin</h1>
-      <p>Frontend developer (React)</p>
       <div>
-        <p>
-          LinkedIn <a href="https://www.linkedin.com/in/aleksandr-kiliushin">aleksandr-kiliushin</a>
-        </p>
-        <p>
-          GitHub <a href="https://github.com/aleksandr-kiliushin">aleksandr-kiliushin</a>
-        </p>
-        <p>
-          E-mail <a href="mailto:aleksandr.kiliushin@gmail.com">aleksandr.kiliushin@gmail.com</a>
-        </p>
-        <p>
-          Telegram <a href="https://t.me/aleksandr_kiliushin">aleksandr_kiliushin</a>
-        </p>
+        <div className={classes.contactsSection}>
+          <h1>Aleksandr Kiliushin</h1>
+          <p>Frontend developer (React)</p>
+          <ul>
+            <li>
+              Icon <a href="mailto:aleksandr.kiliushin@gmail.com">aleksandr.kiliushin@gmail.com</a>
+            </li>
+            <li>
+              Icon <a href="https://www.linkedin.com/in/aleksandr-kiliushin">in/aleksandr-kiliushin</a>
+            </li>
+            <li>
+              Icon <a href="https://github.com/aleksandr-kiliushin">github.com/aleksandr-kiliushin</a>
+            </li>
+            <li>
+              Icon <a href="https://t.me/aleksandr_kiliushin">t.me/aleksandr_kiliushin</a>
+            </li>
+            <li>
+              Icon <a href="https://www.google.com/maps/place/Batumi/">Batumi, Georgia</a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.skillsSection}>Skills</div>
+        <div className={classes.educationSection}>Education</div>
+      </div>
+      <div>
+        <div className={classes.careerObjectiveSection}>Summary</div>
+        <div className={classes.careerObjectiveSection}>Career objective</div>
+        <div className={classes.workExperienceSection}>Work experience</div>
       </div>
     </main>
   )
