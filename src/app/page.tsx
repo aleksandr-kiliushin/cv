a057e01a558f9d1ed70de68e3426a81d5a112398
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-28 10:41:39 +0400

    Add contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,21 @@ const Cv: FC = () => {
   return (
     <main>
       <h1>Aleksandr Kiliushin</h1>
+      <p>Frontend developer (React)</p>
+      <div>
+        <p>
+          LinkedIn <a href="https://www.linkedin.com/in/aleksandr-kiliushin">aleksandr-kiliushin</a>
+        </p>
+        <p>
+          GitHub <a href="https://github.com/aleksandr-kiliushin">aleksandr-kiliushin</a>
+        </p>
+        <p>
+          E-mail <a href="mailto:aleksandr.kiliushin@gmail.com">aleksandr.kiliushin@gmail.com</a>
+        </p>
+        <p>
+          Telegram <a href="https://t.me/aleksandr_kiliushin">aleksandr_kiliushin</a>
+        </p>
+      </div>
     </main>
   )
 }
