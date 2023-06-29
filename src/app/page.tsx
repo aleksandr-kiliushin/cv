33675917679bed3a8a494b83f1deb540e417022d
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-29 21:53:53 +0400

    Set spect ratio A4 paper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,48 +7,57 @@ import portrait from "./portrait.webp"
 const Cv: FC = () => {
   return (
     <div className={classes.cv}>
-      <header className={classes.header}>
-        <Image alt="portrait" className={classes.profilePhoto} src={portrait} />
-        <div className={classes.summary}>
-          <h1>Aleksandr Kiliushin</h1>
-          <p>Frontend developer (React)</p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla veritatis, sapiente optio dicta placeat
-            exercitationem sequi iste! Tempore cum, optio error, ab quod quae fugit saepe asperiores, repellat officiis
-            quas fuga neque! Aspernatur, voluptatem suscipit. Cum, tempora perferendis dolorem numquam facilis quasi
-            adipisci doloribus a saepe debitis minus, dignissimos beatae quo accusamus, quae recusandae velit alias ex
-            itaque quod neque possimus dicta obcaecati! Asperiores rem neque temporibus nisi aspernatur quos debitis
-            modi facilis pariatur cumque deleniti perspiciatis, eum aperiam officiis maxime numquam est accusamus non
-            earum vero itaque fuga. Voluptatum, dolore qui quo repudiandae minus earum dignissimos harum fugit sunt.
-          </p>
-        </div>
-      </header>
-      <div>
-        <div>
-          <div className={classes.contactsSection}>
-            <ul>
-              <li>
-                Icon <a href="mailto:aleksandr.kiliushin@gmail.com">aleksandr.kiliushin@gmail.com</a>
-              </li>
-              <li>
-                Icon <a href="https://www.linkedin.com/in/aleksandr-kiliushin">in/aleksandr-kiliushin</a>
-              </li>
-              <li>
-                Icon <a href="https://github.com/aleksandr-kiliushin">github.com/aleksandr-kiliushin</a>
-              </li>
-              <li>
-                Icon <a href="https://t.me/aleksandr_kiliushin">t.me/aleksandr_kiliushin</a>
-              </li>
-              <li>
-                Icon <a href="https://www.google.com/maps/place/Batumi/">Batumi, Georgia</a>
-              </li>
-            </ul>
-          </div>
-          <div className={classes.skillsSection}>Skills</div>
-          <div className={classes.educationSection}>Education</div>
-          <div className={classes.workExperienceSection}>Work experience</div>
-        </div>
+      <Image alt="portrait" className={classes.profilePhoto} src={portrait} />
+      <div className={classes.summarySection}>
+        <h1>Aleksandr Kiliushin</h1>
+        <p>Frontend developer (React)</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla veritatis, sapiente optio dicta placeat
+          exercitationem sequi iste! Tempore cum, optio error, ab quod quae fugit saepe asperiores, repellat officiis
+          quas fuga neque! Aspernatur, voluptatem suscipit. Cum, tempora perferendis dolorem numquam facilis quasi
+          adipisci doloribus a saepe debitis minus, dignissimos beatae quo accusamus, quae recusandae velit alias ex
+          itaque quod neque possimus dicta obcaecati! Asperiores rem neque temporibus nisi aspernatur quos debitis modi
+          facilis pariatur cumque deleniti perspiciatis, eum aperiam officiis maxime numquam est accusamus non earum
+          vero itaque fuga. Voluptatum, dolore qui quo repudiandae minus earum dignissimos harum fugit sunt.
+        </p>
       </div>
+      <div className={classes.contactsSkillsAndEducationSections}>
+        <div className={classes.contactsSection}>
+          <a className={classes.contact} href="mailto:aleksandr.kiliushin@gmail.com" rel="noreferrer" target="_blank">
+            <span>Icon</span>
+            aleksandr.kiliushin@gmail.com
+          </a>
+          <a
+            className={classes.contact}
+            href="https://www.linkedin.com/in/aleksandr-kiliushin"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <span>Icon</span>
+            in/aleksandr-kiliushin
+          </a>
+          <a className={classes.contact} href="https://github.com/aleksandr-kiliushin" rel="noreferrer" target="_blank">
+            <span>Icon</span>
+            github.com/aleksandr-kiliushin
+          </a>
+          <a className={classes.contact} href="https://t.me/aleksandr_kiliushin" rel="noreferrer" target="_blank">
+            <span>Icon</span>
+            t.me/aleksandr_kiliushin
+          </a>
+          <a
+            className={classes.contact}
+            href="https://www.google.com/maps/place/Batumi"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <span>Icon</span>
+            Batumi, Georgia
+          </a>
+        </div>
+        <section className={classes.skillsSection}>Skills</section>
+        <section className={classes.educationSection}>Education</section>
+      </div>
+      <section className={classes.workExperienceSection}>Work experience</section>
     </div>
   )
 }
