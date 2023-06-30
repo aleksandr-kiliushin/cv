b6c7c634759575a934964da73e1af3d68b94b48e
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-30 20:12:37 +0400

    Set font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { Ubuntu } from "next/font/google"
 import { FC, PropsWithChildren } from "react"
 
 import "./globals.css"
+
+const inter = Ubuntu({ subsets: ["latin"], weight: "400" })
 
 export const metadata = {
   title: "CV | Aleksandr Kiliushin",
@@ -10,7 +13,7 @@ export const metadata = {
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <main>{children}</main>
       </body>
     </html>
